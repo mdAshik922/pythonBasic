c6d27a8695d3b60561b49d67b2952ee5476408a9
mdAshik922 <md.ashik0005723@gmail.com>
2024-01-01 20:25:15 +0600

    mm

--- hh.js
+++ hh.js
@@ -1,7 +1,4 @@
-console.log("kkh",1+1);
 
-let name_K = "ashik";
-console.log(name_K)
-const lol = name_K = "momo";
-console.log(lol);
-console.log(name_K);
+
+const numb = 3 + 3;
+console.log(numb); 
